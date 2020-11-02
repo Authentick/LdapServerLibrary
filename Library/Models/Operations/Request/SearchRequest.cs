705e23568b2763e70598de1516036f9e190e0c4f
Lukas Reschke <lukas@statuscode.ch>
2020-11-02 22:54:47 +0000

    Initial decoding
    Filters besides equal are missing

--- Library/Models/Operations/Request/SearchRequest.cs
+++ Library/Models/Operations/Request/SearchRequest.cs
@@ -14,5 +14,20 @@ namespace LdapServer.Models.Operations.Request
         {
             return 3;
         }
+
+        internal enum ScopeEnum
+        {
+            BaseObject = 0,
+            SingleLevel = 1,
+            WholeSubtree = 2,
+        }
+
+        internal enum DerefAliasesEnum
+        {
+            NeverDerefAliases = 0,
+            DerefInSearching = 1,
+            DerefFindingBaseObj = 2,
+            DerefAlways = 3,
+        }
     }
 }
--- Library/Parser/Decoder/SearchRequestDecoder.cs
+++ Library/Parser/Decoder/SearchRequestDecoder.cs
@@ -8,6 +8,24 @@ namespace LdapServer.Parser.Decoder
     {
         public SearchRequest TryDecode(AsnReader reader)
         {
+            Asn1Tag bindRequestApplication = new Asn1Tag(TagClass.Application, 3);
+            AsnReader subReader = reader.ReadSequence(bindRequestApplication);
+
+            string baseDn = System.Text.Encoding.ASCII.GetString(subReader.ReadOctetString());
+            SearchRequest.ScopeEnum scope = subReader.ReadEnumeratedValue<SearchRequest.ScopeEnum>();
+            SearchRequest.DerefAliasesEnum deref = subReader.ReadEnumeratedValue<SearchRequest.DerefAliasesEnum>();
+            BigInteger sizeLimit = subReader.ReadInteger();
+            BigInteger timeLimit = subReader.ReadInteger();
+            bool typesOnly = subReader.ReadBoolean();
+
+            switch(subReader.PeekTag().TagValue) {
+                case 3:
+                    AsnReader subsubreader = subReader.ReadSequence(new Asn1Tag(TagClass.ContextSpecific, 3));
+                    string attributeDescription = System.Text.Encoding.ASCII.GetString(subsubreader.ReadOctetString());
+                    string assertionValue =  System.Text.Encoding.ASCII.GetString(subsubreader.ReadOctetString());
+                    break;
+            }
+
             SearchRequest searchRequest = new SearchRequest();
 
             return searchRequest;
